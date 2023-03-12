5adb28848df288308cd7269ab12e0e41203bd7e3
Freezerq <thepazdk@gmail.com>
2023-03-12 16:59:57 +0600

    some fixes

--- src/s1-DAL/loginAPI.ts
+++ src/s1-DAL/loginAPI.ts
@@ -3,11 +3,11 @@ import axios from 'axios'
 import { UserType } from './auth-API'
 
 export const instance = axios.create({
-  baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/',
-  // baseURL:
-  //   process.env.NODE_ENV === 'development'
-  //     ? 'http://localhost:7542/2.0/'
-  //     : 'https://neko-back.herokuapp.com/2.0/',
+  // baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/',
+  baseURL:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:7542/2.0/'
+      : 'https://neko-back.herokuapp.com/2.0/',
   withCredentials: true,
 })
 export const loginAPI = {
