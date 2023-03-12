53e2a30d15b9423a82055338ab79ac689100425b
Irina Novakovich <irinaredhood@gmail.com>
2023-03-12 15:45:29 +0300

    fixed imports to slices

--- src/s3-features/Routes/PrivateRoutes.tsx
+++ src/s3-features/Routes/PrivateRoutes.tsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from 'react-router-dom'
 import { useAppSelector } from '../../s1-DAL/store'
 
 export const PrivateRoutes = () => {
-  let isLoggedIn = useAppSelector(state => state.login.isLoggedIn)
+  let isLoggedIn = useAppSelector(state => state.auth.isLoggedIn)
 
   return isLoggedIn ? <Outlet /> : <Navigate to="/" />
 }
