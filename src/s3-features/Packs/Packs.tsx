f898a92276941e955489b3b642a30afc2b4f1490
Irina Novakovich <irinaredhood@gmail.com>
2023-03-13 17:57:21 +0300

    mapping packs

--- src/s3-features/Packs/Packs.tsx
+++ src/s3-features/Packs/Packs.tsx
@@ -51,7 +51,7 @@ export const Packs = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {packs.map(pack => (
+            {packs?.map(pack => (
               <TableRow key={pack._id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
                 <TableCell component="th" scope="row">
                   {pack.name}
