9a540e5a17f2907df9dc84b878d2a0826605d64b
Irina Novakovich <irinaredhood@gmail.com>
2023-03-23 17:23:30 +0300

    pack modals

--- src/s3-features/Packs/PacksTableBody/PacksTableBody.tsx
+++ src/s3-features/Packs/PacksTableBody/PacksTableBody.tsx
@@ -8,7 +8,7 @@ import { useNavigate } from 'react-router-dom'
 
 import { UpdatePackType } from '../../../s1-DAL/packsAPI'
 import { useAppSelector } from '../../../s1-DAL/store'
-import { ActionsForPacks } from '../../Actions/ActionsForPacks'
+import { ActionsForPack } from '../../Actions/ActionsForPack'
 import { PATH } from '../../Routes/AppRoutes'
 
 export const PacksTableBody = (props: PacksTableBodyType) => {
@@ -41,10 +41,11 @@ export const PacksTableBody = (props: PacksTableBodyType) => {
             {appStatus === 'loading' ? <Skeleton /> : pack.user_name}
           </TableCell>
           <TableCell align="left">
-            <ActionsForPacks
-              pack={pack}
-              onDeletePackHandle={props.onDeletePackHandle}
-              onEditPackHandle={props.onEditPackHandle}
+            <ActionsForPack
+              packName={pack.name}
+              packId={pack._id}
+              id={pack.user_id}
+              totalCardsInPack={pack.cardsCount}
             />
           </TableCell>
         </TableRow>
